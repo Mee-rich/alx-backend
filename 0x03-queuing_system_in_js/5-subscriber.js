5897f7db9a04cdb1610fcb53fa151e7db3780e5d
mee-rich <Jamessuyi@gmail.com>
2024-12-03 14:12:04 +0100

    subscribe channel redis

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+#!/usr/bin/yarn dev
+
+/*
+ * Node redis client publisher and subscriber
+ */
+import { createClient} from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.on('error', (err) => console.log('Redis client not connected to the server:', err.toString()));
+
+client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+	console.log(msg);
+	if (msg === EXIT_MSG) {
+		client.unsubscribe();
+		client.quit();
+	}
+});
